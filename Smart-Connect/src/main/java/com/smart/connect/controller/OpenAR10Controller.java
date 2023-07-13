443bfdaa3fa613f8ebb63a90a28f67e3a997bf5d
Pranav More <prnvmore9@gmail.com>
2023-07-13 21:17:48 +0000

    Fixed Action controller issue on com.smart.connect.controller.OpenAR10.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/OpenAR10Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/OpenAR10Controller.java
@@ -1,20 +1,27 @@
 package com.smart.connect.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class OpenAR10Controller {
 
-    @GetMapping(path = "/login")
+    Logger logger = LoggerFactory.getLogger(OpenAR10Controller.class);
+
+    @GetMapping(path = "/OpenAR10/login")
     public String login()
     {
+        logger.error("Error occurred while processing this request.");
         throw new NullPointerException();
     }
 
-    @GetMapping(path = "/successlogin")
+    @GetMapping(path = "/OpenAR10/successlogin")
     public String successLogin()
     {
+        logger.info("This request has been processed successfully.");
         return "Test message";
     }
 }
