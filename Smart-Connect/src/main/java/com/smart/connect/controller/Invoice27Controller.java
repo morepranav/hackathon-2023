565249e05c751e23e89404f3b81010152fc89ab7
Pranav More <prnvmore9@gmail.com>
2023-07-13 21:13:34 +0000

    Fixed Logger class issue on com.smart.connect.controller.Invoice27.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Invoice27Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Invoice27Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Invoice27Controller {
 
-    Logger logger = LoggerFactory.getLogger(Invoice27.class);
+    Logger logger = LoggerFactory.getLogger(Invoice27Controller.class);
 
     @GetMapping(path = "/Invoice27/login")
     public String login()
