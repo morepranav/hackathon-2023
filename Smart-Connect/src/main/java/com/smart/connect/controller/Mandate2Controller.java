5427f5849cc29283367206b8ffb122b94464b88a
Pranjal Bhansali <pranjal.bhansali1@gmail.com>
2023-07-13 18:22:56 +0000

    adding new controller named com.smart.connect.controller.Mandate2.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Mandate2Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Mandate2Controller.java
@@ -19,3 +19,24 @@ public class Mandate2Controller {
     }
 }
 
+package com.smart.connect.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mandate2Controller {
+
+    @GetMapping(path = "/login")
+    public String login()
+    {
+        throw new NullPointerException();
+    }
+
+    @GetMapping(path = "/successlogin")
+    public String successLogin()
+    {
+        return "Test message";
+    }
+}
+
