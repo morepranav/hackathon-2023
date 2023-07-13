a30f6cea05e07440ec3749b28974b87f4cd88d4b
Rohit Madaka <madaka.rohith.reddy@gmail.com>
2023-07-13 19:01:03 +0000

    Fixed deployment controller issue for com.smart.connect.controller.Invoice13.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Invoice13Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Invoice13Controller.java
@@ -22,7 +22,8 @@ public class Invoice13Controller {
     public String successLogin()
     {
         logger.info("This request has been processed successfully.");
-        return "Test message";
+
+	return "Test message";
     }
 }
 
