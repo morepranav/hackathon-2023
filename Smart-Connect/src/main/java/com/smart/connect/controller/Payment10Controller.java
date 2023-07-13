a8bd93968bafe73248345083bfac7a00689f8d2b
Amit Kumar <amitks.kumar@gmail.com>
2023-07-13 19:15:27 +0000

    adding new controller named com.smart.connect.controller.Payment10.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Payment10Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Payment10Controller.java
@@ -1,20 +1,27 @@
 package com.smart.connect.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Payment10Controller {
 
-    @GetMapping(path = "/login")
+    Logger logger = LoggerFactory.getLogger(Payment10.class);
+
+    @GetMapping(path = "//Payment10/login")
     public String login()
     {
+        logger.error("Error occurred while processing this request.");
         throw new NullPointerException();
     }
 
-    @GetMapping(path = "/successlogin")
+    @GetMapping(path = "//Payment10/successlogin")
     public String successLogin()
     {
+        logger.info("This request has been processed successfully.");
         return "Test message";
     }
 }
