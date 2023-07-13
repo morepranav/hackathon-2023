489ad228b5cd5778a0da1e6623f820fabac8f922
Pranav More <pranav-vasant.more@dbtechhackathon.com>
2023-07-13 21:47:28 +0530

    creating new project for hackathon

---
+++ Smart-Connect/src/main/java/com/smart/connect/SmartConnectApplication.java
@@ -0,0 +1,13 @@
+package com.smart.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartConnectApplication.class, args);
+	}
+
+}
---
+++ Smart-Connect/src/test/java/com/smart/connect/SmartConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
