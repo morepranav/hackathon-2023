fb8ae57e48ef7839fab3c887dbe63ade56d7e3ea
Rohit Madaka <madaka.rohith.reddy@gmail.com>
2023-07-13 18:59:38 +0000

    adding new controller named com.smart.connect.controller.Invoice49.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Invoice49Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Invoice49Controller.java
@@ -1,20 +1,27 @@
 package com.smart.connect.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Invoice49Controller {
 
+    Logger logger = LoggerFactory.getLogger(Invoice49.class);
+
     @GetMapping(path = "/login")
     public String login()
     {
+        logger.error("Error occurred while processing this request.");
         throw new NullPointerException();
     }
 
     @GetMapping(path = "/successlogin")
     public String successLogin()
     {
+        logger.info("This request has been processed successfully.");
         return "Test message";
     }
 }
