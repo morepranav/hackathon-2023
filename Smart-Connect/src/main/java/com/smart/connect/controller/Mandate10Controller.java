60edeb44e80c8a1b041d64b4af9553f740812f4c
Pranjal Bhansali <pranjal.bhansali1@gmail.com>
2023-07-13 18:25:53 +0000

    adding new controller named com.smart.connect.controller.Mandate10Controller.java

---
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Mandate10Controller.java
@@ -0,0 +1,21 @@
+package com.smart.connect.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mandate10Controller {
+
+    @GetMapping(path = "/login")
+    public String login()
+    {
+        throw new NullPointerException();
+    }
+
+    @GetMapping(path = "/successlogin")
+    public String successLogin()
+    {
+        return "Test message";
+    }
+}
+
