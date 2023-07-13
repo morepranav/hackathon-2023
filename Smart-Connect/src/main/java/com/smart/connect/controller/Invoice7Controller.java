254dfa96f285cc16c64325576ac7bb9c9e52bd6c
Rohit Madaka <madaka.rohith.reddy@gmail.com>
2023-07-13 19:00:55 +0000

    Fixed deployment controller issue for com.smart.connect.controller.Invoice7.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Invoice7Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Invoice7Controller.java
@@ -22,7 +22,8 @@ public class Invoice7Controller {
     public String successLogin()
     {
         logger.info("This request has been processed successfully.");
-        return "Test message";
+
+	return "Test message";
     }
 }
 
