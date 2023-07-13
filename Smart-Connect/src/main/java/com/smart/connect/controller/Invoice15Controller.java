5c81568c522dcc185e1401df4a40222bc07b69fe
Pranav More <prnvmore9@gmail.com>
2023-07-13 21:10:11 +0000

    Fixed controller start up issue on com.smart.connect.controller.Invoice15.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Invoice15Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Invoice15Controller.java
@@ -9,21 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Invoice15Controller {
 
-    Logger logger = LoggerFactory.getLogger(Invoice15Controller.class);
+    Logger logger = LoggerFactory.getLogger(Invoice15.class);
 
-    @GetMapping(path = "/login")
+    @GetMapping(path = "/Invoice15/login")
     public String login()
     {
         logger.error("Error occurred while processing this request.");
         throw new NullPointerException();
     }
 
-    @GetMapping(path = "/successlogin")
+    @GetMapping(path = "/Invoice15/successlogin")
     public String successLogin()
     {
         logger.info("This request has been processed successfully.");
-
-	return "Test message";
+        return "Test message";
     }
 }
 
