46670dca804c40ce80097746afb920a66c7f085b
Shweta Panicker <shweta16p@gmail.com>
2023-07-13 19:12:35 +0000

    adding new controller named com.smart.connect.controller.Mandate10.java

--- Smart-Connect/src/main/java/com/smart/connect/controller/Mandate10Controller.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/Mandate10Controller.java
@@ -1,20 +1,27 @@
 package com.smart.connect.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Mandate10Controller {
 
-    @GetMapping(path = "/login")
+    Logger logger = LoggerFactory.getLogger(Mandate10.class);
+
+    @GetMapping(path = "//Mandate10/login")
     public String login()
     {
+        logger.error("Error occurred while processing this request.");
         throw new NullPointerException();
     }
 
-    @GetMapping(path = "/successlogin")
+    @GetMapping(path = "//Mandate10/successlogin")
     public String successLogin()
     {
+        logger.info("This request has been processed successfully.");
         return "Test message";
     }
 }
