eb7cae77450a59dafcbc38449cd2759e55fad9a8
Pranav More <pranav-vasant.more@dbtechhackathon.com>
2023-07-14 00:16:54 +0530

    Fixed errors

--- Smart-Connect/src/main/java/com/smart/connect/controller/LoginController.java
+++ Smart-Connect/src/main/java/com/smart/connect/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.smart.connect.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
